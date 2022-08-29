768c10cbf49f460677a3e9d39b4442aab8f487d6
Greentima <blinovtimofey@gmail.com>
2022-08-29 20:26:11 +0300

    solve hw

---
+++ Task41/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] CrArray(int size)
+{
+    int[] NewArray = new int[size];
+    return NewArray;
+}
+
+int[] EnterArrayElements(int[] thisArray) 
+{  
+    Console.WriteLine($"Please, input {thisArray.Length} numbers: "); 
+    for(int i = 0; i < thisArray.Length ; i++)        
+        thisArray[i] = Convert.ToInt32(Console.ReadLine());
+    
+    return thisArray;
+}
+
+void ShowArraysSumPositiveElements(int[] thisArray) 
+{  
+    for(int i = 0; i < thisArray.Length ; i++)
+    {
+        Console.Write(thisArray[i]);
+        if(i < thisArray.Length - 1 )
+            Console.Write(", ");    
+    }
+
+    int EvenNumbers = 0;
+
+    for(int i = 0; i < thisArray.Length ; i++)
+    {
+        if(thisArray[i] > 0)
+        EvenNumbers++;        
+    }
+     if(EvenNumbers > 0)
+        Console.Write($"-> {EvenNumbers}");
+}
+
+Console.WriteLine("You must input M numbers, first of all input M: ");
+int ArSize = Convert.ToInt32(Console.ReadLine());
+int[] MyArray = CrArray(ArSize);
+Console.WriteLine("Array was created :-) ");
+MyArray = EnterArrayElements(MyArray);
+ShowArraysSumPositiveElements(MyArray);
+
---
+++ Task43/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double GetVariable(string varName)
+{
+   Console.WriteLine($"Введите {varName}:");
+   int currentVar = Convert.ToInt32(Console.ReadLine());
+   return currentVar;
+}
+
+Console.WriteLine("Найдём точку пересечения двух прямых,");
+Console.WriteLine("Заданных уравнениями:");
+Console.WriteLine("y = k1 * x + b1");
+Console.WriteLine("y = k2 * x + b2");
+Console.WriteLine("Введите значения b1, k1, b2 и k2");
+double b1 = GetVariable("b1");
+double k1 = GetVariable("k1");
+double b2 = GetVariable("b2");
+double k2 = GetVariable("k2");
+//k1*x + b1 = k2*x + b2
+//k1*x - k2*x = b2 - b1
+//(k1-k2)*x = b2 - b1
+double x = (b2 - b1)/(k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+
+
+
